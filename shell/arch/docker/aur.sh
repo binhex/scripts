76f3b14e065a8a64b82f5e32192aafe7a965f210
binhex <megalith01@gmail.com>
2016-11-02 13:37:19 +0000

    remove hack, now using compiled helper

--- shell/arch/docker/aur.sh
+++ shell/arch/docker/aur.sh
@@ -3,23 +3,9 @@
 # exit script if return code != 0
 set -e
 
-# create "makepkg-user" user for makepkg
-useradd -m -s /bin/bash makepkg-user
-echo -e "makepkg-password\nmakepkg-password" | passwd makepkg-user
-
-# prevent prompt for password when running makepkg
-echo "makepkg-user ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
-
-# download aur helper tarball
-curl -L -o "/home/makepkg-user/${aur_helper}.tar.gz" "https://aur.archlinux.org/cgit/aur.git/snapshot/${aur_helper}.tar.gz"
-cd /home/makepkg-user
-su -c "tar -xvf ${aur_helper}.tar.gz" - makepkg-user
-
-# compile aur helper using makepkg (as non root)
-su -c "cd /home/makepkg-user/${aur_helper} && makepkg -s --noconfirm --needed" - makepkg-user
-
-# install compiled package using pacman (as root)
-pacman -U /home/makepkg-user/${aur_helper}/${aur_helper}*.pkg.tar.xz --noconfirm
+# manually download aur helper from binhex repo
+curl -o "/tmp/${aur_helper}-any.pkg.tar.xz" -L "https://github.com/binhex/arch-packages/raw/master/compiled/${aur_helper}-any.pkg.tar.xz"
+pacman -U "/tmp/${aur_helper}-any.pkg.tar.xz" --noconfirm
 
 # install app using aur helper (as root)
 "${aur_helper}" -S "${aur_packages}" --noconfirm || true

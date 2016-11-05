2b517b52f6c44c52a25740195d0f6634ff0fa1e7
binhex <megalith01@gmail.com>
2016-11-05 23:18:23 +0000

    detect aor pkg repo and arch using jq

--- shell/arch/docker/aor.sh
+++ shell/arch/docker/aor.sh
@@ -3,8 +3,13 @@
 # exit script if return code != 0
 set -e
 
+# get repo and arch from aor using api (json format)
+aor_package_results=$(curl "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&arch=any&arch=x86_64" | jq ".results[0] | { repo: .repo, arch: .arch}")
+aor_package_repo=$(echo $aor_package_results | jq ".repo")
+aor_package_arch=$(echo $aor_package_results | jq ".arch")
+
 # get latest compiled package from aor (required due to the fact we use archive snapshot)
 if [[ ! -z "${aor_packages}" ]]; then
-	curl -L -o "/tmp/${aor_packages}.tar.xz" "https://www.archlinux.org/packages/${aor_package_type}/any/${aor_packages}/download/"
+	curl -L -o "/tmp/${aor_packages}.tar.xz" "https://www.archlinux.org/packages/${aor_package_repo}/${aor_package_arch}/${aor_package_name}/download/"
 	pacman -U "/tmp/${aor_packages}.tar.xz" --noconfirm
 fi

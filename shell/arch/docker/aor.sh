d8843e72eb85a56987edf9e3cd8b1a931e734c91
binhex <megalith01@gmail.com>
2016-11-06 00:07:06 +0000

    add debug, switch to raw jq string

--- shell/arch/docker/aor.sh
+++ shell/arch/docker/aor.sh
@@ -5,11 +5,16 @@ set -e
 
 # get repo and arch from aor using api (json format)
 aor_package_results=$(curl "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&arch=any&arch=x86_64" | jq ".results[0] | { repo: .repo, arch: .arch}")
-aor_package_repo=$(echo $aor_package_results | jq ".repo")
-aor_package_arch=$(echo $aor_package_results | jq ".arch")
+
+aor_package_repo=$(echo $aor_package_results | jq -r ".repo")
+echo "AOR package repo: ${aor_package_repo}"
+
+aor_package_arch=$(echo $aor_package_results | jq -r ".arch")
+echo "AOR package arch: ${aor_package_arch}"
 
 # get latest compiled package from aor (required due to the fact we use archive snapshot)
 if [[ ! -z "${aor_package_name}" ]]; then
+	echo "curl -L -o "/tmp/${aor_package_name}.tar.xz" "https://www.archlinux.org/packages/${aor_package_repo}/${aor_package_arch}/${aor_package_name}/download/""
 	curl -L -o "/tmp/${aor_package_name}.tar.xz" "https://www.archlinux.org/packages/${aor_package_repo}/${aor_package_arch}/${aor_package_name}/download/"
 	pacman -U "/tmp/${aor_package_name}.tar.xz" --noconfirm
 fi

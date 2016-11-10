3300e318afa6328737c0c37d8c9d5d27f7046709
binhex <megalith01@gmail.com>
2016-11-10 16:53:42 +0000

    confirm yes to any prompts from pacman

--- shell/arch/docker/aur.sh
+++ shell/arch/docker/aur.sh
@@ -7,7 +7,7 @@ set -e
 if [[ ! -z "${aur_packages}" ]]; then
 	curl -o "/tmp/${aur_helper}-any.pkg.tar.xz" -L "https://github.com/binhex/arch-packages/raw/master/compiled/${aur_helper}-any.pkg.tar.xz"
 	pacman -U "/tmp/${aur_helper}-any.pkg.tar.xz" --noconfirm
-	"${aur_helper}" -S ${aur_packages} --noconfirm || true
+	yes|"${aur_helper}" -S ${aur_packages} --noconfirm || true
 fi
 
 # remove base devel excluding useful core packages

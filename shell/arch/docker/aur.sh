fcf8e817550a3d37eb595a3a4cd739d637ed2c08
binhex <megalith01@gmail.com>
2016-11-11 13:49:02 +0000

    capture exit code from apacman

--- shell/arch/docker/aur.sh
+++ shell/arch/docker/aur.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-# exit script if return code != 0
-set -e
-
 # install aur helper from github and then install app using helper
 if [[ ! -z "${aur_packages}" ]]; then
 	curl -o "/tmp/${aur_helper}-any.pkg.tar.xz" -L "https://github.com/binhex/arch-packages/raw/master/compiled/${aur_helper}-any.pkg.tar.xz"
 	pacman -U "/tmp/${aur_helper}-any.pkg.tar.xz" --noconfirm
-	yes|"${aur_helper}" -S ${aur_packages} --noconfirm || true
+	"${aur_helper}" -S ${aur_packages} --noconfirm
+else
+	exit 0
+fi
+
+exit_code=$?
+
+if (( ${exit_code} != 0 && ${exit_code} != 1 )); then
+	echo "apacman returned exit code ${exit_code} (exit code 1 ignored), showing man for exit codes:-"
+	cat << EOM
+	0   Success
+	1   Miscellaneous errors
+	2   Invalid parameters
+	3   Fatal errors, not warnings
+	4   No package matches found
+	5   Package does not exist
+	6   No internet connection
+	7   No free space in tmpfs
+	8   One or more package(s) failed to build, keep going
+	9   One package failed to build, do not continue
+	10  Permission problem −− fakeroot
+	11  Permission problem −− root user
+	12  Permission problem −− sudo
+	13  Permission problem −− su
+EOM
+
+	# set exit code to 1 to denote failure to build env
+	exit 1
 fi
 
 # remove base devel excluding useful core packages

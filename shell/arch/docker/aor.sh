1a8dcc328e7eb80e99649edcb168efe0e8233c63
binhex <megalith01@gmail.com>
2017-04-15 23:20:44 +0100

    filter out testing repos for aor

--- shell/arch/docker/aor.sh
+++ shell/arch/docker/aor.sh
@@ -12,7 +12,7 @@ if [[ ! -z "${aor_packages}" ]]; then
 	for aor_package_name in "${aor_package_list[@]}"; do
 
 		# get repo and arch from aor using api (json format)
-		aor_package_results=$(curl "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&arch=any&arch=x86_64" | jq ".results[0] | { repo: .repo, arch: .arch}")
+		aor_package_results=$(curl "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&repo=Community&repo=Core&repo=Extra&repo=Multilib&arch=any&arch=x86_64" | jq ".results[0] | { repo: .repo, arch: .arch}")
 
 		aor_package_repo=$(echo $aor_package_results | jq -r ".repo")
 		echo "AOR package repo: ${aor_package_repo}"

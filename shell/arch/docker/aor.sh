9dcab145aa755806ec1d10a9250f08bd088a7269
binhex <megalith01@gmail.com>
2017-04-26 18:35:58 +0100

    prevent fuzzy match for aor packages

--- shell/arch/docker/aor.sh
+++ shell/arch/docker/aor.sh
@@ -12,12 +12,15 @@ if [[ ! -z "${aor_packages}" ]]; then
 	for aor_package_name in "${aor_package_list[@]}"; do
 
 		# get repo and arch from aor using api (json format)
-		aor_package_results=$(curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&repo=Community&repo=Core&repo=Extra&repo=Multilib&arch=any&arch=x86_64" | jq ".results[0] | { repo: .repo, arch: .arch}")
+		aor_json=$(curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&repo=Community&repo=Core&repo=Extra&repo=Multilib&arch=any&arch=x86_64")
 
-		aor_package_repo=$(echo $aor_package_results | jq -r ".repo")
+		# filter based on exact package name to prevent fuzzy matching of wrong packages
+		aor_package_json=$(echo "${aor_json}" | jq -c --arg aor_package_name "$aor_package_name" '.results[] | select(.pkgname | startswith($aor_package_name) and endswith($aor_package_name))')
+
+		aor_package_repo=$(echo $aor_package_json | jq -r ".repo")
 		echo "AOR package repo: ${aor_package_repo}"
 
-		aor_package_arch=$(echo $aor_package_results | jq -r ".arch")
+		aor_package_arch=$(echo $aor_package_json | jq -r ".arch")
 		echo "AOR package arch: ${aor_package_arch}"
 
 		# get latest compiled package from aor (required due to the fact we use archive snapshot)

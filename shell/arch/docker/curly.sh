1f8b2d11a2b3788170b877f3239d31b0920c7ea1
binhex <megalith01@gmail.com>
2017-10-12 17:03:19 +0100

    add in flag to resume when transfer fails

--- shell/arch/docker/curly.sh
+++ shell/arch/docker/curly.sh
@@ -30,7 +30,7 @@ function run_curl() {
 
 	while true; do
 
-		response_code=$(curl --connect-timeout 5 --max-time 10 --retry "${retry_count}" --retry-delay "${retry_wait}" --retry-max-time "${retry_max_time}" -o "${output_file}" -L "${silent_mode}" -w "%{http_code}" "${url}")
+		response_code=$(curl --continue-at - --connect-timeout 5 --max-time 10 --retry "${retry_count}" --retry-delay "${retry_wait}" --retry-max-time "${retry_max_time}" -o "${output_file}" -L "${silent_mode}" -w "%{http_code}" "${url}")
 		exit_code=$?
 
 		if [ "${response_code}" -ge "200" ] && [ "${response_code}" -le "299" ]; then

f1ba638f4a7302d15d444fce5d96d7d93f935986
binhex <megalith01@gmail.com>
2017-10-25 13:20:20 +0100

    update path to curly

--- shell/arch/docker/aor.sh
+++ shell/arch/docker/aor.sh
@@ -14,7 +14,7 @@ if [[ ! -z "${aor_packages}" ]]; then
 		echo "[info] attempting download for aor package ${aor_package_name}"
 		
 		# get repo and arch from aor using api (json format)
-		curly.sh -rc 6 -rw 10 -of /tmp/aor_json -url "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&repo=Community&repo=Core&repo=Extra&repo=Multilib&arch=any&arch=x86_64"
+		/root/curly.sh -rc 6 -rw 10 -of /tmp/aor_json -url "https://www.archlinux.org/packages/search/json/?q=${aor_package_name}&repo=Community&repo=Core&repo=Extra&repo=Multilib&arch=any&arch=x86_64"
 
 		# filter based on exact package name to prevent fuzzy matching of wrong packages
 		aor_package_json=$(cat /tmp/aor_json | jq -c --arg aor_package_name "$aor_package_name" '.results[] | select(.pkgname | startswith($aor_package_name) and endswith($aor_package_name))')
@@ -28,8 +28,8 @@ if [[ ! -z "${aor_packages}" ]]; then
 		# get latest compiled package from aor (required due to the fact we use archive snapshot)
 		if [[ ! -z "${aor_package_repo}" && ! -z "${aor_package_arch}" ]]; then
 
-			echo "[info] curly.sh -rc 6 -rw 10 -of /tmp/${aor_package_name}.tar.xz -url https://www.archlinux.org/packages/${aor_package_repo}/${aor_package_arch}/${aor_package_name}/download/"
-			curly.sh -rc 6 -rw 10 -of "/tmp/${aor_package_name}.tar.xz" -url "https://www.archlinux.org/packages/${aor_package_repo}/${aor_package_arch}/${aor_package_name}/download/"
+			echo "[info] /root/curly.sh -rc 6 -rw 10 -of /tmp/${aor_package_name}.tar.xz -url https://www.archlinux.org/packages/${aor_package_repo}/${aor_package_arch}/${aor_package_name}/download/"
+			/root/curly.sh -rc 6 -rw 10 -of "/tmp/${aor_package_name}.tar.xz" -url "https://www.archlinux.org/packages/${aor_package_repo}/${aor_package_arch}/${aor_package_name}/download/"
 			pacman -U "/tmp/${aor_package_name}.tar.xz" --noconfirm
 
 		else

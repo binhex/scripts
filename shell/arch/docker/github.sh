c6e79e440f87df193408eaa3d3ce0f59e9a5e028
binhex <megalith01@gmail.com>
2017-11-03 16:41:28 +0000

    mark new script as executable

---
+++ shell/arch/docker/github.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+# this script downloads github releases in zipped format, extracts and cleans up
+
+# exit script if return code != 0
+set -e
+
+# setup default values
+readonly ourScriptName=$(basename -- "$0")
+readonly defaultDownloadFilename="github-download.zip"
+readonly defaultDownloadPath="/tmp"
+readonly defaultExtractPath="/tmp/extracted"
+
+download_filename="${defaultDownloadFilename}"
+download_path="${defaultDownloadPath}"
+download_full_path="${download_path}/${download_filename}"
+extract_path="${defaultExtractPath}"
+
+function github_downloader() {
+
+	echo -e "[info] Running script to download latest release from GitHub..."
+
+	mkdir -p "${download_path}"
+	mkdir -p "${extract_path}"
+	mkdir -p "${install_path}"
+
+	github_release_tags_url="https://github.com/${github_owner}/${github_repo}/releases"
+
+	echo -e "[info] Downloading GitHub release tags from url ${github_release_tags_url}..."
+	/root/curly.sh -rc 6 -rw 10 -of "${download_path}/release_tag" -url "${github_release_tags_url}"
+
+	release_tag=$(cat ${download_path}/release_tag | grep -P -o -m 1 "(?<=/${github_owner}/${github_repo}/releases/tag/)[^\"]+")
+	echo -e "[info] Release tag from GitHub is ${release_tag}"
+
+	github_release_url="https://github.com/${github_owner}/${github_repo}/archive/${release_tag}.zip"
+
+	echo -e "[info] Downloading release from GitHub url ${github_release_url}, saving to ${download_full_path}..."
+	/root/curly.sh -rc 6 -rw 10 -of "${download_full_path}" -url "${github_release_url}"
+
+	echo -e "[info] Extracting to ${extract_path} ..."
+	unzip -o "${download_full_path}" -d "${extract_path}"
+
+	echo -e "[info] Moving to install path ${install_path} ..."
+	mv -f "${extract_path}/${github_repo}"*/* "${install_path}/"
+
+	echo -e "[info] Removing source archive from ${download_full_path} ..."
+	rm -f "${download_full_path}"
+
+}
+
+function show_help() {
+	cat <<ENDHELP
+Description:
+	Script to download GitHub releases.
+Syntax:
+	${ourScriptName} [args]
+Where:
+	-h or --help
+		Displays this text.
+
+	-df or --download-filename <filename.ext>
+		Define name of the downloaded file
+		Defaults to '${defaultDownloadFilename}'.
+
+	-dp or --download-path <path>
+		Define path to download to.
+		Defaults to '${defaultDownloadPath}'.
+
+	-ep or --extract-path <path>
+		Define path to extract the download to.
+		Defaults to '${defaultExtractPath}'.
+
+	-ip or --install-path <path>
+		Define path to install to.
+		No default.
+
+	-go or --github-owner <owner>
+		Define GitHub owners name.
+		No default.
+
+	-gr or --github-repo <repo>
+		Define GitHub repository name.
+		No default.
+Example:
+	./github.sh -df github-download.zip -dp /tmp -ep /tmp/extracted -ip /opt/binhex/deluge -go binhex -gr arch-deluge
+ENDHELP
+}
+
+while [ "$#" != "0" ]
+do
+	case "$1"
+	in
+		-df|--download-filename)
+			download_filename=$2
+			shift
+			;;
+		-dp| --download-path)
+			download_path=$2
+			shift
+			;;
+		-ep|extract-path)
+			extract_path=$2
+			shift
+			;;
+		-ip|--install-path)
+			install_path=$2
+			shift
+			;;
+		-go|--github-owner)
+			github_owner=$2
+			shift
+			;;
+		-gr|--github-repo)
+			github_repo=$2
+			shift
+			;;
+		-h|--help)
+			show_help
+			exit 0
+			;;
+		*)
+			echo "${ourScriptName}: ERROR: Unrecognised argument '$1'." >&2
+			show_help
+			 exit 1
+			 ;;
+	 esac
+	 shift
+done
+
+# check we have mandatory parameters, else exit with warning
+if [[ -z "${install_path}" ]]; then
+	echo "[warning] Install path not defined via parameter -ip or --install-path, displaying help..."
+	show_help
+	exit 1
+fi
+
+if [[ -z "${github_owner}" ]]; then
+	echo "[warning] GitHub owner's name not defined via parameter -go or --github-owner, displaying help..."
+	show_help
+	exit 1
+fi
+
+if [[ -z "${github_repo}" ]]; then
+	echo "[warning] GitHub repo name not defined via parameter -gr --github-repo, displaying help..."
+	show_help
+	exit 1
+fi
+
+github_downloader "$Pdownload_filename}" "${download_path}" "${extract_path}" "${install_path}" "${github_owner}" "${github_repo}"

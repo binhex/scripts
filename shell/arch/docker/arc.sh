47bb274d5d2c96f71c42d50b1610891000cc6c8b
binhex <megalith01@gmail.com>
2018-07-13 12:52:35 +0100

    remove hard coded arch, could be any

--- shell/arch/docker/arc.sh
+++ shell/arch/docker/arc.sh
@@ -5,7 +5,7 @@ set -e
 
 if [[ ! -z "${arc_packages}" ]]; then
 
-	# split space seperated string of packages into list
+	# split space separated string of packages into list
 	IFS=' ' read -ra arc_package_list <<< "${arc_packages}"
 
 	# process each package in the list
@@ -20,8 +20,8 @@ if [[ ! -z "${arc_packages}" ]]; then
 
 		echo "[info] Attempting download for archive package '${arc_package_name}' ver ${arc_package_version}"
 		
-		echo "[info] /root/curly.sh -rc 6 -rw 10 -of /tmp/${arc_package_name}.tar.xz -url https://archive.archlinux.org/packages/${arc_package_name_first_letter}/${arc_package_name}/${arc_package_name}-${arc_package_version}-x86_64.pkg.tar.xz"
-		/root/curly.sh -rc 6 -rw 10 -of "/tmp/${arc_package_name}.tar.xz" -url "https://archive.archlinux.org/packages/${arc_package_name_first_letter}/${arc_package_name}/${arc_package_name}-${arc_package_version}-x86_64.pkg.tar.xz"
+		echo "[info] /root/curly.sh -rc 6 -rw 10 -of /tmp/${arc_package_name}.tar.xz -url https://archive.archlinux.org/packages/${arc_package_name_first_letter}/${arc_package_name}/${arc_package_name}-${arc_package_version}.pkg.tar.xz"
+		/root/curly.sh -rc 6 -rw 10 -of "/tmp/${arc_package_name}.tar.xz" -url "https://archive.archlinux.org/packages/${arc_package_name_first_letter}/${arc_package_name}/${arc_package_name}-${arc_package_version}.pkg.tar.xz"
 		pacman -U "/tmp/${arc_package_name}.tar.xz" --noconfirm
 
 	done

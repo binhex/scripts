d7d3a037ebe655204495921c8c91d718c878226d
binhex <megalith01@gmail.com>
2018-10-24 10:47:17 +0100

    remove aur helper if it already exists

--- shell/arch/docker/aur.sh
+++ shell/arch/docker/aur.sh
@@ -8,6 +8,10 @@ aur_helper_version="3.1-1"
 if [[ ! -z "${aur_packages}" ]]; then
 
 	pacman -S --needed base-devel --noconfirm
+
+	# remove existing aur helper if it exists to prevent curl 416 error
+	rm -f "/tmp/${aur_helper}-any.pkg.tar.xz"
+
 	/root/curly.sh -rc 6 -rw 10 -of "/tmp/${aur_helper}-any.pkg.tar.xz" -url "https://github.com/binhex/arch-packages/raw/master/compiled/${aur_helper}-${aur_helper_version}-any.pkg.tar.xz"
 	pacman -U "/tmp/${aur_helper}-any.pkg.tar.xz" --noconfirm
 

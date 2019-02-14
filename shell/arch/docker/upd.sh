6041998c81ead4fafc07c5e96462a8de54755e38
binhex <megalith01@gmail.com>
2019-02-14 12:14:33 +0000

    add in update script

---
+++ shell/arch/docker/upd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+echo "[info] Updating mirrorlist for pacman using reflector..."
+
+# use reflector to overwriting existing mirrorlist, args explained below
+# --sort rate                       = sort by download rate
+# --age 1                           = Only return mirrors that have synchronized in the last 1 hours.
+# --latest 5                        = Limit the list to the 5 most recently synchronized servers.
+# --score 5                         = Limit the list to the n servers with the highest score.
+# --save /etc/pacman.d/mirrorlist   = Save the mirrorlist to the given path.
+pacman -S reflector --noconfirm
+reflector --connection-timeout 60 --cache-timeout 60 --sort rate --age 1 --latest 5 --score 5 --save /etc/pacman.d/mirrorlist
+pacman -Rs reflector --noconfirm
+
+# sync package databases for pacman
+pacman -Syyu --noconfirm

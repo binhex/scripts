39453ef79c0589042c49a90db8e46a0d5f0858ac
binhex <megalith01@gmail.com>
2019-06-05 15:27:17 +0100

    set perms on converted file

--- shell/arch/docker/dos2unix-wrapper.sh
+++ shell/arch/docker/dos2unix-wrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # wrapper script to fix up NFS mount issues with dos2unix
+# see here for details https://access.redhat.com/solutions/3168671
 # ${1} should be path to file to convert
 
 # check the file exists
@@ -16,9 +17,13 @@ crlf_line_ending_count=$(cat -e "${1}" | grep -c '\^M\$')
 
 # note we use dos2unix -n to prevent issues when file is located on NFS mount
 if [ "${crlf_line_ending_count}" != 0 ]; then
-    echo "[info] '${1}' contains CRLF (Windows) line endings, converting to LF (Unix)..." | ts '%Y-%m-%d %H:%M:%.S'
-    rm -f "/tmp/${filename}-lf.tmp"
-    dos2unix -n "${1}" "/tmp/${filename}-lf.tmp" 2> /dev/null
-    mv -f "/tmp/${filename}-lf.tmp" "${1}"
-    echo "[info] Line ending conversion complete" | ts '%Y-%m-%d %H:%M:%.S'
+	echo "[info] '${1}' contains CRLF (Windows) line endings, converting to LF (Unix)..." | ts '%Y-%m-%d %H:%M:%.S'
+	rm -f "/tmp/${filename}-lf.tmp"
+	dos2unix -n "${1}" "/tmp/${filename}-lf.tmp" 2> /dev/null
+	mv -f "/tmp/${filename}-lf.tmp" "${1}"
+	echo "[info] Line ending conversion complete" | ts '%Y-%m-%d %H:%M:%.S'
+	echo "[info] Setting permissions on converted file '${1}'..." | ts '%Y-%m-%d %H:%M:%.S'
+	chown -R "${PUID}":"${PGID}" "${1}"
+	chmod -R 775 "${1}"
+	echo "[info] Permissions set" | ts '%Y-%m-%d %H:%M:%.S'
 fi

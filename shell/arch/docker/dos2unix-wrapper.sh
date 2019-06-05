cd96efbdffb38289b58b4aac687690956458c0ec
binhex <megalith01@gmail.com>
2019-06-05 11:26:56 +0100

    ensure temp filename is not overwritten/deleted

--- shell/arch/docker/dos2unix-wrapper.sh
+++ shell/arch/docker/dos2unix-wrapper.sh
@@ -8,14 +8,17 @@ if [ ! -f "${1}" ]; then
     exit 1
 fi
 
+# get the filename without path, used for temp filename
+filename=$(basename -- "${1}")
+
 # check whether the file is set to CRLF line endings
 exit_code=$(cat -e "${1}" | grep '\^M\$')
 
 # note we use dos2unix -n to prevent issues when file is located on NFS mount
 if [ "${exit_code}" == 0 ]; then
     echo "[info] '${1}' contains CRLF (Windows) line endings, converting to LF (Unix)..." | ts '%Y-%m-%d %H:%M:%.S'
-    rm -f "/tmp/converted-lf.tmp"
-    dos2unix -n "${1}" "/tmp/converted-lf.tmp" 1> /dev/null
-    mv -f "/tmp/converted-lf.tmp" "${1}"
+    rm -f "/tmp/${filename}-lf.tmp"
+    dos2unix -n "${1}" "/tmp/${filename}-lf.tmp" 1> /dev/null
+    mv -f "/tmp/${filename}-lf.tmp" "${1}"
     echo "[info] Line ending conversion complete" | ts '%Y-%m-%d %H:%M:%.S'
 fi

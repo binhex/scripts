6d2ccc2fc90a3819e7da79f44bcf53ff1f72842e
binhex <megalith01@gmail.com>
2019-06-05 10:53:29 +0100

    add dos2unix wrapper script

---
+++ shell/arch/docker/dos2unix-wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# wrapper script to fix up NFS mount issues with dos2unix
+# ${1} should be path to file to convert
+
+# check the file exists
+if [ ! -f "${1}" ]; then
+    exit 1
+fi
+
+# check whether the file is set to CRLF line endings
+exit_code=$(cat -e "${1}" | grep '\^M\$')
+
+# note we use dos2unix -n to prevent issues when file is located on NFS mount
+if [ "${exit_code}" == 0 ]; then
+    echo "[info] '${1}' contains CRLF (Windows) line endings, converting to LF (Unix)..." | ts '%Y-%m-%d %H:%M:%.S'
+    rm -f "/tmp/converted-lf.tmp"
+    dos2unix -n "${1}" "/tmp/converted-lf.tmp" 1> /dev/null
+    mv -f "/tmp/converted-lf.tmp" "${1}"
+    echo "[info] Line ending conversion complete" | ts '%Y-%m-%d %H:%M:%.S'
+fi

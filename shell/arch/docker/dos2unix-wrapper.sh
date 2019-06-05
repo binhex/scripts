5143b7f75989b9cc6683ae520d770eb2025bf9f8
binhex <megalith01@gmail.com>
2019-06-05 15:06:37 +0100

    need to use grep line count not exit code

--- shell/arch/docker/dos2unix-wrapper.sh
+++ shell/arch/docker/dos2unix-wrapper.sh
@@ -12,13 +12,13 @@ fi
 filename=$(basename -- "${1}")
 
 # check whether the file is set to CRLF line endings
-exit_code=$(cat -e "${1}" | grep '\^M\$')
+crlf_line_ending_count=$(cat -e "${1}" | grep -c '\^M\$')
 
 # note we use dos2unix -n to prevent issues when file is located on NFS mount
-if [ "${exit_code}" == 0 ]; then
+if [ "${crlf_line_ending_count}" != 0 ]; then
     echo "[info] '${1}' contains CRLF (Windows) line endings, converting to LF (Unix)..." | ts '%Y-%m-%d %H:%M:%.S'
     rm -f "/tmp/${filename}-lf.tmp"
-    dos2unix -n "${1}" "/tmp/${filename}-lf.tmp" 1> /dev/null
+    dos2unix -n "${1}" "/tmp/${filename}-lf.tmp" 2> /dev/null
     mv -f "/tmp/${filename}-lf.tmp" "${1}"
     echo "[info] Line ending conversion complete" | ts '%Y-%m-%d %H:%M:%.S'
 fi

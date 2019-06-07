128a771eb72240925e23fa9fac532c0dd8834413
binhex <megalith01@gmail.com>
2019-06-07 12:45:57 +0100

    rip out workaround for dos2unix -n, not req

--- shell/arch/docker/dos2unix-wrapper.sh
+++ shell/arch/docker/dos2unix-wrapper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# wrapper script to fix up NFS mount issues with dos2unix
-# see here for details https://access.redhat.com/solutions/3168671
+# wrapper script to perform check before forcing conversion of line endings
 # ${1} should be path to file to convert
 
 # check the file exists
@@ -9,21 +8,11 @@ if [ ! -f "${1}" ]; then
     exit 1
 fi
 
-# get the filename without path, used for temp filename
-filename=$(basename -- "${1}")
-
 # check whether the file is set to CRLF line endings
 crlf_line_ending_count=$(cat -e "${1}" | grep -c '\^M\$')
 
-# note we use dos2unix -n to prevent issues when file is located on NFS mount
 if [ "${crlf_line_ending_count}" != 0 ]; then
 	echo "[info] '${1}' contains CRLF (Windows) line endings, converting to LF (Unix)..." | ts '%Y-%m-%d %H:%M:%.S'
-	rm -f "/tmp/${filename}-lf.tmp"
-	dos2unix -n "${1}" "/tmp/${filename}-lf.tmp" 2> /dev/null
-	mv -f "/tmp/${filename}-lf.tmp" "${1}"
+	dos2unix "${1}"
 	echo "[info] Line ending conversion complete" | ts '%Y-%m-%d %H:%M:%.S'
-	echo "[info] Setting permissions on converted file '${1}'..." | ts '%Y-%m-%d %H:%M:%.S'
-	chown -R "${PUID}":"${PGID}" "${1}"
-	chmod -R 775 "${1}"
-	echo "[info] Permissions set" | ts '%Y-%m-%d %H:%M:%.S'
 fi

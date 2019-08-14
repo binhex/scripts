b9642c3595224726a1a435b99c1fb483638d2f08
binhex <megalith01@gmail.com>
2019-08-14 14:31:01 +0100

    mark new script as executable

---
+++ shell/arch/docker/cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# identify if base-devel package installed
+if pacman -Qi "base-devel" > /dev/null ; then
+
+	# remove base devel excluding useful core packages
+	pacman -Ru $(pacman -Qgq base-devel | grep -v awk | grep -v pacman | grep -v sed | grep -v grep | grep -v gzip | grep -v which) --noconfirm
+
+fi
+
+# general cleanup
+yes|pacman -Scc
+pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
+rm -rf /usr/share/locale/*
+rm -rf /usr/share/man/*
+rm -rf /usr/share/gtk-doc/*
+rm -rf /tmp/*

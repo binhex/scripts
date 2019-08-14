f75b38b0d73f5ab4a2b208a2215ecd405aa0b1e4
binhex <megalith01@gmail.com>
2019-08-14 16:32:03 +0100

    base-devel group detect fix

--- shell/arch/docker/cleanup.sh
+++ shell/arch/docker/cleanup.sh
@@ -4,7 +4,7 @@
 set -e
 
 # identify if base-devel package installed
-if pacman -Qi "base-devel" > /dev/null ; then
+if pacman -Qg "base-devel" > /dev/null ; then
 
 	# remove base devel excluding useful core packages
 	pacman -Ru $(pacman -Qgq base-devel | grep -v awk | grep -v pacman | grep -v sed | grep -v grep | grep -v gzip | grep -v which) --noconfirm
@@ -13,7 +13,7 @@ fi
 
 # general cleanup
 yes|pacman -Scc
-pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
+ 2> /dev/null || true
 rm -rf /usr/share/locale/*
 rm -rf /usr/share/man/*
 rm -rf /usr/share/gtk-doc/*

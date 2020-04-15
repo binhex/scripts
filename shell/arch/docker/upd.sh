9c1c0475f69aa63cfce7d72f6267ba9224f2e176
binhex <megalith01@gmail.com>
2020-04-15 11:12:24 +0100

    retry code for reflector

--- shell/arch/docker/upd.sh
+++ shell/arch/docker/upd.sh
@@ -3,7 +3,7 @@
 # exit script if return code != 0
 set -e
 
-echo "[info] Updating mirrorlist for pacman using reflector..."
+pacman -S reflector --noconfirm
 
 # use reflector to overwriting existing mirrorlist, args explained below
 # --sort rate                       = sort by download rate
@@ -11,8 +11,39 @@ echo "[info] Updating mirrorlist for pacman using reflector..."
 # --latest 5                        = Limit the list to the 5 most recently synchronized servers.
 # --score 5                         = Limit the list to the n servers with the highest score.
 # --save /etc/pacman.d/mirrorlist   = Save the mirrorlist to the given path.
-pacman -S reflector --noconfirm
-reflector --connection-timeout 60 --cache-timeout 60 --sort rate --age 1 --latest 5 --score 5 --save /etc/pacman.d/mirrorlist
+# mirrorlist does not always populate, retry if exit code not 0
+
+echo "[info] Updating mirrorlist for pacman using reflector..."
+retry_count=5
+while true; do
+
+	reflector --connection-timeout 60 --cache-timeout 60 --sort rate --age 1 --latest 5 --score 5 --save /etc/pacman.d/mirrorlist
+	exit_code=$?
+
+	if [[ "${exit_code}" -ne "0" ]]; then
+
+		retry_count=$((retry_count-1))
+
+		if [ "${retry_count}" -eq "0" ]; then
+
+			echo "[crit] Failed to download mirrorlist, too many retries" ; exit 1
+
+		else
+
+			echo '[warn] Failed to download mirrorlist, sleeping for 60 seconds before retrying...'
+
+		fi
+
+		sleep 60s
+
+	else
+
+		echo "[info] Successfully downloaded the mirrorlist"
+		break
+
+	fi
+
+done
 
 echo "[info] Show contents of reflector generated mirrorlist for pacman..."
 cat /etc/pacman.d/mirrorlist

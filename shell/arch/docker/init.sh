9609116e9a88e64663fef38b5c3392e036557df8
binhex <megalith01@gmail.com>
2020-05-01 10:34:38 +0100

    create env var for arch based on os-release

--- shell/arch/docker/init.sh
+++ shell/arch/docker/init.sh
@@ -24,6 +24,19 @@ fi
 
 echo "[info] System information $(uname -a)" | ts '%Y-%m-%d %H:%M:%.S'
 
+export OS_ARCH=$(cat /etc/os-release | grep -P -o -m 1 "(?=^ID\=).*" | grep -P -o -m 1 "[a-z]+$")
+if [[ ! -z "${OS_ARCH}" ]]; then
+	if [ "${OS_ARCH}" -eq "arch" ]; then
+		OS_ARCH="x86-64"
+	else
+		OS_ARCH="aarch64"
+	fi
+	echo "[info] OS_ARCH defined as '${OS_ARCH}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[warn] Unable to identify OS_ARCH, defaulting to 'x86-64'" | ts '%Y-%m-%d %H:%M:%.S'
+	export OS_ARCH="x86-64"
+fi
+
 export PUID=$(echo "${PUID}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
 if [[ ! -z "${PUID}" ]]; then
 	echo "[info] PUID defined as '${PUID}'" | ts '%Y-%m-%d %H:%M:%.S'

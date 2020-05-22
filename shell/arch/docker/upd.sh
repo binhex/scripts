cc90c8b3872a06a49287401160bd4d5ea2834246
binhex <megalith01@gmail.com>
2020-05-22 14:58:43 +0100

    try needed flag

--- shell/arch/docker/upd.sh
+++ shell/arch/docker/upd.sh
@@ -69,4 +69,4 @@ cat "/etc/pacman.conf"
 # see below for details:-
 # https://www.archlinux.org/news/nss3511-1-and-lib32-nss3511-1-updates-require-manual-intervention/
 echo "[info] Synchronize pacman database and then upgrade any existing packages using pacman..."
-pacman -Syu --overwrite /usr/lib\*/p11-kit-trust.so --noconfirm
+pacman -Syyu --overwrite /usr/lib\*/p11-kit-trust.so --needed  --noconfirm

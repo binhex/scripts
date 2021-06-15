cfbba651b2aebf0f4954a6c4f92a5805378f6fce
binhex <megalith01@gmail.com>
2021-06-15 11:54:40 +0100

    move symlink to utils.sh

--- shell/arch/docker/init.sh
+++ shell/arch/docker/init.sh
@@ -8,10 +8,10 @@ exec 3>&1 4>&2 &> >(tee -a /config/supervisord.log)
 
 cat << "EOF"
 Created by...
-___.   .__       .__                   
+___.   .__       .__
 \_ |__ |__| ____ |  |__   ____ ___  ___
  | __ \|  |/    \|  |  \_/ __ \\  \/  /
- | \_\ \  |   |  \   Y  \  ___/ >    < 
+ | \_\ \  |   |  \   Y  \  ___/ >    <
  |___  /__|___|  /___|  /\___  >__/\_ \
      \/        \/     \/     \/      \/
    https://hub.docker.com/u/binhex/
@@ -111,34 +111,8 @@ else
 
 fi
 
-function symlink_home_dir {
-
-	app_name="${1}"
-
-	# if container folder exists then rename and use as default restore
-	if [[ -d "/home/nobody" && ! -L "/home/nobody" ]]; then
-		echo "[info] /home/nobody folder storing user general settings already exists, renaming..."
-		mv "/home/nobody" "/home/nobody-backup"
-	fi
-
-	# if /config/home doesnt exist then restore from backup (see note above)
-	if [[ ! -d "/config/${app_name}/home" ]]; then
-		if [[ -d "/home/nobody-backup" ]]; then
-			echo "[info] /config/${app_name}/home folder storing user general settings does not exist, copying defaults..."
-			mkdir -p "/config/${app_name}/home" ; cp -R "/home/nobody-backup/"* "/config/${app_name}/home"
-		fi
-	else
-		echo "[info] /config/${app_name}/home folder storing user general settings already exists, skipping copy"
-	fi
-
-	# create soft link to /home/nobody/${folder} storing general settings
-	echo "[info] Creating soft link from /config/${app_name}/home to /home/nobody..."
-	mkdir -p "/config/${app_name}/home" ; rm -rf "/home/nobody" ; ln -s "/config/${app_name}/home/" "/home/nobody"
-
-	# reset permissions after file copy
-	chown -R "${PUID}":"${PGID}" "/config/${app_name}/home" "/home/nobody"
-
-}
+# source in utilities script
+source '/usr/local/bin/utils.sh'
 
 # ENVVARS_COMMON_PLACEHOLDER
 

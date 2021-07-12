3d7d9e7a93534d99d51ff9310d399438f9e6a124
binhex <megalith01@gmail.com>
2021-07-12 10:41:17 +0100

    enhance pip for requirements or package name

--- shell/arch/docker/pip.sh
+++ shell/arch/docker/pip.sh
@@ -1,39 +1,127 @@
 #!/bin/bash
 
+# set defaults
+defaultLogLevel="WARN"
+log_level="${defaultLogLevel}"
+
 # logger function
 source '/usr/local/bin/utils.sh'
 
-# check path param exists
-if [[ -z "${1}" ]]; then
-    logger "First parameter for path not defined" "ERROR"
+function pip_install() {
+
+    # define pacman packages
+    pacman_packages="python python-pip"
+
+    # install compiled packages using pacman
+    if [[ -n "${pacman_packages}" ]]; then
+        pacman -S --needed $pacman_packages --noconfirm
+    fi
+
+    cd "${install_path}" || logger "Cannot change to path '${install_path}'" "ERROR"
+
+    if [[ -z "${pip_packages}" ]]; then
+
+        if [[ ! -f "${install_path}/requirements.txt" ]]; then
+            logger "Path to requirements.txt '${install_path}/requirements.txt' does not exist, showing help..." "WARN"
+            show_help
+            return 1
+        fi
+
+        # install virtualenv, create env and activate
+        python3 -m pip install --user virtualenv
+        python3 -m venv env
+        source "${install_path}/env/bin/activate"
+
+        # install python modules as per requirements.txt in virtualenv
+        pip install -r "${install_path}/requirements.txt"
+
+    else
+
+        # install virtualenv, create env and activate
+        python3 -m pip install --user virtualenv
+        python3 -m venv env
+        source "${install_path}/env/bin/activate"
+
+        # install python package in virtualenv
+        pip install -U "${pip_packages}"
+    fi
+
+}
+
+function show_help() {
+	cat <<ENDHELP
+Description:
+	A script to install Python pre-requisites via 'requirements.txt' or pip install.
+Syntax:
+	./pip.sh [args]
+Where:
+	-h or --help
+		Displays this text.
+
+	-ip or --install-path <path>
+		Define path to installation.
+		No default.
+
+	-pp or --pip-paackages <path>
+		Define destinaiton path to store files copied from src-path,
+		this is then symlinked back (src-path renamed to *-backup).
+		No default.
+
+	-ll or --log-level <DEBUG|INFO|WARN|ERROR>
+		Define logging level.
+		Defaults to '${defaultLogLevel}'.
+
+Examples:
+	Install Python modules using requirements.txt file:
+		./pip.sh --install-path /opt/sickchill --log-level 'WARN'
+
+	Install Python application using pip:
+		./pip.sh --install-path /opt/sickchill --pip-packages sickchill --log-level 'WARN'
+
+ENDHELP
+}
+
+while [ "$#" != "0" ]
+do
+    case "$1"
+    in
+        -rq|--install-path)
+            install_path=$2
+            shift
+            ;;
+        -pp|--pip-packages)
+            pip_packages=$2
+            shift
+            ;;
+        -ll|--log-level)
+            log_level=$2
+            shift
+            ;;
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        *)
+            echo "[WARN] Unrecognised argument '$1', displaying help..." >&2
+            echo ""
+            show_help
+            return 1
+            ;;
+    esac
+    shift
+done
+
+# verify required options specified
+if [[ -z "${install_path}" ]]; then
+    logger "Install path not specified, showing help..." "WARN"
+    show_help
+    return 1
 fi
 
-install_path="${1}"
-
-# check path exists
 if [[ ! -d "${install_path}" ]]; then
-    logger "Path '${install_path}' does not exist" "ERROR"
+    logger "Install path '${install_path}' does not exist, showing help..." "WARN"
+    show_help
+    return 1
 fi
 
-# define pacman packages
-pacman_packages="python python-pip"
-
-# install compiled packages using pacman
-if [[ ! -z "${pacman_packages}" ]]; then
-    pacman -S --needed $pacman_packages --noconfirm
-fi
-
-cd "${install_path}" || logger "Cannot change to path '${install_path}'" "ERROR"
-
-# install virtualenv, create env and activate
-python3 -m pip install --user virtualenv
-python3 -m venv env
-source "${install_path}/env/bin/activate"
-
-# install python modules as per requirements.txt in virtualenv
-pip install -r "${install_path}/requirements.txt"
-
-# install required packages
-pip install -r requirements.txt
-
-logger "Please specify 'cd ${install_path} && source './env/bin/activate' && python3 ${install_path}/<name of python script> <options...>' as commonad to run" "INFO"
+pip_install

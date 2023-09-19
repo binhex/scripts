ca310d4b792e1322e0bd11b83839ba1cbcababa6
binhex <megalith01@gmail.com>
2023-09-19 11:28:07 +0100

    remove hacks and rework for multi arch

--- shell/arch/docker/upd.sh
+++ shell/arch/docker/upd.sh
@@ -5,6 +5,7 @@ set -e
 
 function create_static_mirrorlist() {
 
+if [[ "${TARGETARCH}" == "amd64" ]]; then
 cat <<'EOF' > /etc/pacman.d/mirrorlist
 Server = https://arch.mirror.constant.com/$repo/os/$arch
 Server = https://arch.mirror.square-r00t.net/$repo/os/$arch
@@ -12,70 +13,86 @@ Server = http://arch.mirror.square-r00t.net/$repo/os/$arch
 Server = rsync://arch.mirror.constant.com/archlinux/$repo/os/$arch
 Server = rsync://arch.mirror.square-r00t.net/arch/$repo/os/$arch
 EOF
+else
+cat <<'EOF' > /etc/pacman.d/mirrorlist
+Server = http://mirror.archlinuxarm.org/$arch/$repo
+EOF
+fi
 
 }
 
-pacman -S reflector --noconfirm
+function run_reflector() {
 
-# use reflector to overwriting existing mirrorlist, args explained below
-# --sort age                        = sort by last server synchronized
-# --age 1                           = Only return mirrors that have synchronized in the last 1 hours.
-# --latest 5                        = Limit the list to the 5 most recently synchronized servers.
-# --score 5                         = Limit the list to the n servers with the highest score.
-# --save /etc/pacman.d/mirrorlist   = Save the mirrorlist to the given path.
-# mirrorlist does not always populate, retry if exit code not 0
+	pacman -S reflector --noconfirm
 
-echo "[info] Updating mirrorlist for pacman using reflector..."
-retry_count=3
-sleep_period_secs=10
-completion_percent=100
+	# use reflector to overwriting existing mirrorlist, args explained below
+	# --sort age                        = sort by last server synchronized
+	# --age 1                           = Only return mirrors that have synchronized in the last 1 hours.
+	# --latest 5                        = Limit the list to the 5 most recently synchronized servers.
+	# --score 5                         = Limit the list to the n servers with the highest score.
+	# --save /etc/pacman.d/mirrorlist   = Save the mirrorlist to the given path.
+	# mirrorlist does not always populate, retry if exit code not 0
 
-while true; do
+	echo "[info] Updating mirrorlist for pacman using reflector..."
+	retry_count=3
+	sleep_period_secs=10
+	completion_percent=100
 
-	# required, as this script is sourced in and thus picks up set -e
-	set +e
-	reflector_stderr=$(reflector --completion-percent="${completion_percent}" --connection-timeout 60 --cache-timeout 60 --sort age --age 1 --latest 5 --score 5 --save /etc/pacman.d/mirrorlist 2>&1)
-	set -e
-	exit_code=$?
+	while true; do
 
-	if [[ ! -z "${reflector_stderr}" ]]; then
-		echo "[info] reflector stderr is '${reflector_stderr}'"
-	fi
+		# required, as this script is sourced in and thus picks up set -e
+		set +e
+		reflector_stderr=$(reflector --completion-percent="${completion_percent}" --connection-timeout 60 --cache-timeout 60 --sort age --age 1 --latest 5 --score 5 --save /etc/pacman.d/mirrorlist 2>&1)
+		set -e
+		exit_code=$?
 
-	echo "[info] reflector exit code is '${exit_code}'"
+		if [[ ! -z "${reflector_stderr}" ]]; then
+			echo "[info] reflector stderr is '${reflector_stderr}'"
+		fi
 
-	if [[ "${reflector_stderr}" == *"error"* || "${exit_code}" != "0" ]]; then
+		echo "[info] reflector exit code is '${exit_code}'"
 
-		retry_count=$((retry_count-1))
-		completion_percent=$((completion_percent-1))
+		if [[ "${reflector_stderr}" == *"error"* || "${exit_code}" != "0" ]]; then
 
-		if [ "${retry_count}" -eq "0" ]; then
+			retry_count=$((retry_count-1))
+			completion_percent=$((completion_percent-1))
 
-			echo "[warn] Failed to download mirrorlist, too many retries, falling back to static list"
-			create_static_mirrorlist
-			break
+			if [ "${retry_count}" -eq "0" ]; then
+
+				echo "[warn] Failed to download mirrorlist, too many retries, falling back to static list"
+				create_static_mirrorlist
+				break
+
+			else
+
+				echo "[warn] Failed to download mirrorlist, ${retry_count} retries left"
+				echo "[info] Reducing completion percentage to ${completion_percent}%"
+				echo "[info] Sleeping for ${sleep_period_secs} seconds..."
+
+			fi
+
+			sleep "${sleep_period_secs}s"
 
 		else
 
-			echo "[warn] Failed to download mirrorlist, ${retry_count} retries left"
-			echo "[info] Reducing completion percentage to ${completion_percent}%"
-			echo "[info] Sleeping for ${sleep_period_secs} seconds..."
+			echo "[info] Successfully downloaded the mirrorlist"
+			break
 
 		fi
 
-		sleep "${sleep_period_secs}s"
+	done
 
-	else
+	echo "[info] Show contents of reflector generated mirrorlist for pacman..."
+	cat /etc/pacman.d/mirrorlist
 
-		echo "[info] Successfully downloaded the mirrorlist"
-		break
+}
 
-	fi
-
-done
-
-echo "[info] Show contents of reflector generated mirrorlist for pacman..."
-cat /etc/pacman.d/mirrorlist
+# reflector only supported for amd64, use static mirrorlist for arm64
+if [[ "${TARGETARCH}" == "amd64" ]]; then
+	run_reflector
+else
+	create_static_mirrorlist
+fi
 
 if [[ ! -z "${pacman_ignore_packages}" ]]; then
 
@@ -94,20 +111,10 @@ fi
 echo "[info] Showing pacman configuration file '/etc/pacman.conf'..."
 cat "/etc/pacman.conf"
 
-# note overwrite required due to bug in missing soname link
-# see below for details:-
-# https://www.archlinux.org/news/nss3511-1-and-lib32-nss3511-1-updates-require-manual-intervention/
 echo "[info] Synchronize pacman database and then upgrade any existing packages using pacman..."
 
 if [[ "${pacman_confirm}" == "yes" ]]; then
-	yes|pacman -Syyu --overwrite /usr/lib\*/p11-kit-trust.so
+	yes|pacman -Syyu
 else
-	pacman -Syyu --overwrite /usr/lib\*/p11-kit-trust.so --noconfirm
+	pacman -Syyu --noconfirm
 fi
-
-# delme once fixed!!
-# force downgrade of coreutils - fixes permission denied issue when building on docker hub
-# https://github.com/archlinux/archlinux-docker/issues/32
-curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/coreutils.tar.xz -L "https://github.com/binhex/packages/raw/master/compiled/x86-64/coreutils.tar.xz"
-pacman -U '/tmp/coreutils.tar.xz' --noconfirm
-# /delme once fixed!!

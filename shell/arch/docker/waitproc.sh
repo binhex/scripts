cec8d1abb4c7cfe9e3b6998e4954bb64ba959c0d
binhex <megalith01@gmail.com>
2023-11-13 12:34:59 +0000

    marking file as executable

---
+++ shell/arch/docker/waitproc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function child_process_monitor(){
+	echo "[info] Waiting for child processes to exit..."
+
+	while true; do
+		child_pid=$(jobs -rp)
+		if [[ -z "${child_pid}" ]]; then
+			echo "[info] All child processes exited"
+			break
+		fi
+		sleep 0.5s
+		echo "[info] Child process with pid id '${child_pid}' still running, waiting..."
+	done
+}
+
+# monitor for child processes on intialised shutdown
+trap 'child_process_monitor' SIGTERM SIGINT EXIT

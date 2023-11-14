79285e2fa7a12093ae835428766495f35600a07a
binhex <megalith01@gmail.com>
2023-11-14 15:27:22 +0000

    enhance wait script to capture sigint and exit

--- shell/arch/docker/waitproc.sh
+++ shell/arch/docker/waitproc.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# script name
+readonly ourScriptName="$(basename -- "$0")"
+
 function child_process_monitor(){
 	echo "[info] Waiting for child processes to exit..."
 
 	while true; do
 		child_pid=$(jobs -rp)
 		if [[ -z "${child_pid}" ]]; then
-			echo "[info] All child processes exited"
+			echo "[info] All child processes exited, exiting while loop..."
 			break
 		fi
 		sleep 0.5s
 		echo "[info] Child process with pid id '${child_pid}' still running, waiting..."
 	done
+	echo "[info] All child processes exited, exiting '${ourScriptName}' script..."
+	exit
 }
 
 # monitor for child processes on intialised shutdown
-trap 'child_process_monitor' SIGTERM
+trap 'child_process_monitor' SIGTERM SIGINT

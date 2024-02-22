cbf4a8211b9ec1fa5ddb30e8d1d2f622780f294a
binhex <megalith01@gmail.com>
2024-02-22 13:34:51 +0000

    add url for flaresolverr

--- shell/arch/docker/testrunner.sh
+++ shell/arch/docker/testrunner.sh
@@ -246,6 +246,11 @@ function run_test() {
 		# run tests
 		webui_test ${common_options} --container-ports '-p 9999:8096'
 
+	elif [[ "${app_name}" == "flaresolverr" ]]; then
+
+		# run tests
+		webui_test ${common_options} --url 'v1' --container-ports '-p 9999:8191'
+
 	elif [[ "${app_name}" == "jackett" ]]; then
 
 		# force jackett to listen on ipv4

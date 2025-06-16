3d09062448c2d417ac1d844f7835623dd49eed77
binhex <megalith01@gmail.com>
2025-06-16 09:59:24 +0100

    lots of changes to add in env var support

--- shell/arch/docker/portset.sh
+++ shell/arch/docker/portset.sh
@@ -11,15 +11,22 @@ readonly ourScriptName="$(basename -- "$0")"
 readonly ourScriptversion="1.0.0"
 
 # default values
-readonly defaultDebug="no"
+readonly defaultQbittorrentConfigFilepath="/config/qBittorrent/config/qBittorrent.conf"
+readonly defaultQbittorrentWebuiPort="8080"
+readonly defaultQbittorrentBindAdapter="yes"
 readonly defaultGluetunControlServerPort="8000"
+readonly defaultConfigureIncomingPort="yes"
 readonly defaultPollDelay="10"
-readonly defaultBindAdapter="yes"
+readonly defaultDebug="no"
 
-debug="${defaultDebug}"
-qbittorrent_bind_adapter="${defaultBindAdapter}"
-gluetun_control_server_port="${defaultGluetunControlServerPort}"
-poll_delay="${defaultPollDelay}"
+# read env var values if not empty, else use defaults
+QBITTORRENT_CONFIG_FILEPATH="${QBITTORRENT_CONFIG_FILEPATH:-${defaultQbittorrentConfigFilepath}}"
+QBITTORRENT_WEBUI_PORT="${APPLICATION_PORT:-${defaultQbittorrentWebuiPort}}"
+QBITTORRENT_BIND_ADAPTER="${QBITTORRENT_BIND_ADAPTER:-${defaultQbittorrentBindAdapter}}"
+GLUETUN_CONTROL_SERVER_PORT="${GLUETUN_CONTROL_SERVER_PORT:-${defaultGluetunControlServerPort}}"
+CONFIGURE_INCOMING_PORT="${CONFIGURE_INCOMING_PORT:-${defaultConfigureIncomingPort}}"
+POLL_DELAY="${POLL_DELAY:-${defaultPollDelay}}"
+DEBUG="${DEBUG:-${defaultDebug}}"
 
 # Read all command line arguments
 SCRIPT_ARGS=("$@")
@@ -28,8 +35,7 @@ SCRIPT_ARGS=("$@")
 remaining_args=()
 
 function incoming_port_watchdog {
-	local control_server_url="http://127.0.0.1:${gluetun_control_server_port}/v1"
-	local vpn_current_incoming_port
+	local control_server_url="http://127.0.0.1:${GLUETUN_CONTROL_SERVER_PORT}/v1"
 	local vpn_previous_incoming_port
 	local vpn_public_ip
 	local vpn_country_ip
@@ -40,86 +46,99 @@ function incoming_port_watchdog {
 		exit 1
 	fi
 
-	if [[ "${debug}" == "yes" ]]; then
+	if [[ "${DEBUG}" == "yes" ]]; then
 		echo "[DEBUG] Successfully connected to gluetun Control Server at '${control_server_url}'"
 	fi
 
 	while true; do
 
-		vpn_current_incoming_port=$(curl -s "${control_server_url}/openvpn/portforwarded" | jq -r '.port')
+		INCOMING_PORT=$(curl -s "${control_server_url}/openvpn/portforwarded" | jq -r '.port')
 		vpn_public_ip=$(curl -s "${control_server_url}/publicip/ip" | jq -r '.public_ip')
 		vpn_country_ip=$(curl -s "${control_server_url}/publicip/ip" | jq -r '.country')
 		vpn_city_ip=$(curl -s "${control_server_url}/publicip/ip" | jq -r '.city')
 
-		if [[ "${debug}" == "yes" ]]; then
-			echo "[DEBUG] Current incoming port for VPN tunnel is '${vpn_current_incoming_port}'"
+		if [[ "${DEBUG}" == "yes" ]]; then
+			echo "[DEBUG] Current incoming port for VPN tunnel is '${INCOMING_PORT}'"
 			echo "[DEBUG] Public IP for VPN tunnel is '${vpn_public_ip}'"
 			echo "[DEBUG] Country for VPN tunnel is '${vpn_country_ip}'"
 			echo "[DEBUG] City for VPN tunnel is '${vpn_city_ip}'"
 		fi
 
-		if [[ "${vpn_current_incoming_port}" != "${vpn_previous_incoming_port}" ]]; then
+		if [[ "${INCOMING_PORT}" != "${vpn_previous_incoming_port}" ]]; then
 			if [[ -z "${vpn_previous_incoming_port}" ]]; then
 				echo "[INFO] No previous VPN port forward found, assuming first run, configuring application..."
 			else
-				echo "[INFO] Previous VPN port forward '${vpn_previous_incoming_port}' and current VPN port forward '${vpn_current_incoming_port}' are different, configuring application..."
+				echo "[INFO] Previous VPN port forward '${vpn_previous_incoming_port}' and current VPN port forward '${INCOMING_PORT}' are different, configuring application..."
 			fi
 
 			# Start new application process with new port
-			application_configure "${vpn_current_incoming_port}"
-			vpn_previous_incoming_port="${vpn_current_incoming_port}"
+			application_configure
+			vpn_previous_incoming_port="${INCOMING_PORT}"
 		else
-				if [[ "${debug}" == "yes" ]]; then
-					echo "[DEBUG] Previous VPN port forward '${vpn_previous_incoming_port}' and current VPN port forward '${vpn_current_incoming_port}' are the same, nothing to do."
+				if [[ "${DEBUG}" == "yes" ]]; then
+					echo "[DEBUG] Previous VPN port forward '${vpn_previous_incoming_port}' and current VPN port forward '${INCOMING_PORT}' are the same, nothing to do."
 				fi
 		fi
 
 		# Sleep for a bit before checking again
-		if [[ "${debug}" == "yes" ]]; then
-			echo "[DEBUG] sleeping for '${poll_delay}' seconds before next invocation..."
+		if [[ "${DEBUG}" == "yes" ]]; then
+			echo "[DEBUG] sleeping for '${POLL_DELAY}' seconds before next invocation..."
 		fi
-		sleep "${poll_delay}"
+		sleep "${POLL_DELAY}"
 	done
 }
 
 function kill_process() {
 	# Kill existing application process if it exists
 	if [[ -n "${APPLICATION_PID}" ]] && kill -0 "${APPLICATION_PID}" 2>/dev/null; then
-		echo "[INFO] Killing existing application process with PID: ${APPLICATION_PID}"
+		echo "[INFO] Killing existing application process with PID '${APPLICATION_PID}'"
 		kill "${APPLICATION_PID}"
 		wait "${APPLICATION_PID}" 2>/dev/null
-		echo "[INFO] Application process with PID ${APPLICATION_PID} has been killed"
+		echo "[INFO] Application process with PID '${APPLICATION_PID}' has been killed"
 	fi
 }
 
 function nicotineplus_start_process() {
-	echo "[INFO] Starting ${APPLICATION_NAME} with incoming port: $incoming_port"
-	"${SCRIPT_ARGS[@]}" --port "${incoming_port}" &
+	echo "[INFO] Starting '${APPLICATION_NAME}' with incoming port '${INCOMING_PORT}'"
+	"${SCRIPT_ARGS[@]}" --port "${INCOMING_PORT}" &
 	APPLICATION_PID=$!
-	echo "[INFO] Started ${APPLICATION_NAME} with PID '${APPLICATION_PID}'"
+	echo "[INFO] Started '${APPLICATION_NAME}' with PID '${APPLICATION_PID}'"
 }
 
 function nicotineplus_configure_incoming_port() {
 	kill_process
-	echo "[INFO] Configuring ${APPLICATION_NAME} with VPN incoming port: $incoming_port"
+	echo "[INFO] Configuring '${APPLICATION_NAME}' with VPN incoming port: ${INCOMING_PORT}"
 	nicotineplus_start_process
 }
 
 function qbittorrent_configure_incoming_port() {
-	echo "[INFO] Configuring ${APPLICATION_NAME} with VPN incoming port: $incoming_port"
+
+	local web_protocol
+
+	echo "[INFO] Configuring '${APPLICATION_NAME}' with VPN incoming port '${INCOMING_PORT}'"
+
+		# identify protocol, used by curl to connect to api
+		if grep -q 'WebUI\\HTTPS\\Enabled=true' "${QBITTORRENT_CONFIG_FILEPATH}"; then
+			web_protocol="https"
+		else
+			web_protocol="http"
+		fi
+
+		# note -k flag required to support insecure connection (self signed certs) when https used
+		curl -k -i -X POST -d "json={\"random_port\": false}" "${web_protocol}://localhost:${QBITTORRENT_WEBUI_PORT}/api/v2/app/setPreferences" &> /dev/null
+		curl -k -i -X POST -d "json={\"listen_port\": ${INCOMING_PORT}}" "${web_protocol}://localhost:${QBITTORRENT_WEBUI_PORT}/api/v2/app/setPreferences" &> /dev/null
+
 }
 
 function qbittorrent_configure_bind_adapter() {
-	if [[ "${qbittorrent_bind_adapter,,}" == 'yes' ]]; then
-		echo "[INFO] Binding ${APPLICATION_NAME} to gluetun network interface"
+	if [[ "${QBITTORRENT_BIND_ADAPTER,,}" == 'yes' ]]; then
+		echo "[INFO] Binding '${APPLICATION_NAME}' to gluetun network interface"
 	else
-		echo "[INFO] Not binding ${APPLICATION_NAME} to gluetun network interface"
+		echo "[INFO] Not binding '${APPLICATION_NAME}' to gluetun network interface"
 	fi
 }
 
 function application_configure() {
-	local incoming_port="$1"
-	shift
 
 	if [[ "${APPLICATION_NAME,,}" == 'nicotineplus' ]]; then
 		nicotineplus_configure_incoming_port
@@ -142,17 +161,33 @@ Syntax:
   ./${ourScriptName} [options] [command and arguments]
 
 Where:
+  -an or --application-name <name>
+		Define the name of the application to configure for incoming port.
+		Defaults to '${APPLICATION_NAME}'.
+
+  -qcf or --qbittorrent-config-filepath <path>
+		Define the file path to the qBittorrent configuration file.
+		Defaults to '${QBITTORRENT_CONFIG_FILEPATH}'.
+
+  -qwp or --qbittorrent-webui-port <port>
+		Define the web UI port for qBittorrent.
+		Defaults to '${QBITTORRENT_WEBUI_PORT}'.
+
   -qba or --qbittorrent-bind-adapter <yes|no>
 		Define whether to bind qBittorrent to the gluetun network interface.
-		Defaults to '${qbittorrent_bind_adapter}'.
+		Defaults to '${QBITTORRENT_BIND_ADAPTER}'.
 
   -gcsp or --gluetun-control-server-port <port>
 		Define the Gluetun Control Server port.
-		Defaults to '${gluetun_control_server_port}'.
+		Defaults to '${GLUETUN_CONTROL_SERVER_PORT}'.
+
+  -cip or --configure-incoming-port <yes|no>
+		Define whether to enable VPN port monitoring and application configuration.
+		Defaults to '${CONFIGURE_INCOMING_PORT}'.
 
   -pd or --poll-delay <seconds>
 		Define the polling delay in seconds between incoming port checks.
-		Defaults to '${poll_delay}'.
+		Defaults to '${POLL_DELAY}'.
 
   --debug
 		Define whether debug mode is enabled.
@@ -162,11 +197,24 @@ Where:
 		Displays this text.
 
 Environment Variables:
-  CONFIGURE_INCOMING_PORT
-		Set to 'yes' to enable VPN port monitoring and application configuration.
 	APPLICATION_NAME
+		Set the name of the application to configure with the VPN incoming port.
+  QBITTORRENT_CONFIG_FILEPATH
+		Set the file path to the qBittorrent configuration file.
+  QBITTORRENT_WEBUI_PORT
+		Set the web UI port for qBittorrent.
+	QBITTORRENT_BIND_ADAPTER
 		Set to the name of the application to configure with the VPN incoming port.
-		Example: 'nicotineplus' or 'qbittorrent'.
+	GLUETUN_CONTROL_SERVER_PORT
+		Set the port for the Gluetun Control Server.
+	CONFIGURE_INCOMING_PORT
+		Set to 'yes' to enable VPN port monitoring and application configuration.
+	POLL_DELAY
+		Set the polling delay in seconds between incoming port checks.
+	DEBUG
+		Set to 'yes' to enable debug mode.
+Notes:
+  - Command line arguments take precedence over environment variables.
 
 Examples:
   Monitor VPN port and configure Nicotine+:
@@ -181,8 +229,6 @@ Examples:
 	Manually executing the script for debug:
 	CONFIGURE_INCOMING_PORT=yes APPLICATION_NAME=nicotineplus ./${ourScriptName} --debug /usr/bin/nicotine
 
-Notes:
-  - Env Vars 'CONFIGURE_INCOMING_PORT' and 'APPLICATION_NAME' are normally set by the container runtime.
 ENDHELP
 }
 
@@ -190,20 +236,36 @@ while [ "$#" != "0" ]
 do
   case "$1"
   in
+	-an|--application-name)
+		APPLICATION_NAME="${2}"
+    shift
+    ;;
+  -qcf|--qbittorrent-config-filepath)
+		QBITTORRENT_CONFIG_FILEPATH="${2}"
+    shift
+    ;;
+  -qwp|--qbittorrent-webui-port)
+		QBITTORRENT_WEBUI_PORT="${2}"
+    shift
+    ;;
   -qba|--qbittorrent-bind-adapter)
-		qbittorrent_bind_adapter="${2}"
+		QBITTORRENT_BIND_ADAPTER="${2}"
     shift
     ;;
   -gcsp|--gluetun-control-server-port)
-		gluetun_control_server_port="${2}"
+		GLUETUN_CONTROL_SERVER_PORT="${2}"
     shift
     ;;
+  -cipd|--configure-incoming-port)
+		CONFIGURE_INCOMING_PORT="${2}"
+		shift
+		;;
   -pd|--poll-delay)
-		poll_delay="${2}"
+		POLL_DELAY="${2}"
 		shift
 		;;
   --debug)
-    debug="yes"
+    DEBUG="yes"
     ;;
   -h|--help)
     show_help

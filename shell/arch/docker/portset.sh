7f73b4dec78d967504b90ba273064687505f4a8d
binhex <megalith01@gmail.com>
2025-07-03 13:02:43 +0100

    add in sleep msg

--- shell/arch/docker/portset.sh
+++ shell/arch/docker/portset.sh
@@ -308,6 +308,7 @@ function main {
       echo "[INFO] Previous VPN port forward '${PREVIOUS_INCOMING_PORT}' and current VPN port forward '${INCOMING_PORT}' are the same, checking again in ${POLL_DELAY} seconds..."
     fi
 
+    echo "[INFO] Sleeping for ${POLL_DELAY} seconds before checking again..."
     sleep "${POLL_DELAY}"
   done
 }

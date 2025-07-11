35570b57baa961073ab9bc7c2c006d84129e384d
binhex <megalith01@gmail.com>
2025-07-11 11:52:32 +0100

    use var for path

--- shell/docker/refresh.sh
+++ shell/docker/refresh.sh
@@ -5,33 +5,35 @@
 
 set -e
 
+scripts_dest_path="/usr/local/bin"
+
 # ensure git cli is installed
 pacman -S git --noconfirm
 
 # silence warning aobut permissions
-git config --global --add safe.directory '/usr/local/bin'
+git config --global --add safe.directory "${scripts_dest_path}"
 
-# if scripts repository does not exist, clone it
-if [[ ! -d '/usr/local/bin/shell' ]]; then
+# if scripts path is not a git repository then clone
+if git -C "${scripts_dest_path}" rev-parse --is-inside-work-tree &>/dev/null; then
 	# create directory for scripts repository
-	mkdir -p /usr/local/bin/
+	mkdir -p "${scripts_dest_path}"
 	# git clone scripts repository
-	git clone https://github.com/binhex/scripts --depth 1 '/usr/local/bin'
+	git clone https://github.com/binhex/scripts --depth 1 "${scripts_dest_path}"
 fi
 
 # ensure scripts repository is up to date
-cd /usr/local/bin || exit 1
+cd "${scripts_dest_path}" || exit 1
 git pull || true
 
 # add docker scripts to PATH
-if ! grep -q '/usr/local/bin/shell/docker' '/root/.bashrc' &>/dev/null; then
-	echo 'export PATH="/usr/local/bin/shell/docker:${PATH}"' >> '/root/.bashrc'
+if ! grep -q "${scripts_dest_path}/shell/docker" '/root/.bashrc' &>/dev/null; then
+	echo "export PATH=\"${scripts_dest_path}/shell/docker:\${PATH}\"" >> '/root/.bashrc'
 fi
 
-if ! grep -q '/usr/local/bin/shell/docker' '/home/nobody/.bashrc' &>/dev/null; then
-	echo 'export PATH="/usr/local/bin/shell/docker:${PATH}"' >> '/home/nobody/.bashrc'
+if ! grep -q "${scripts_dest_path}/shell/docker" '/home/nobody/.bashrc' &>/dev/null; then
+	echo "export PATH=\"${scripts_dest_path}/shell/docker:\${PATH}\"" >> '/home/nobody/.bashrc'
 fi
 
 # ensure scripts are executable and owned by nobody:users
-chown -R nobody:users '/usr/local/bin'
-chmod -R 755 '/usr/local/bin'
+chown -R nobody:users "${scripts_dest_path}"
+chmod -R 755 "${scripts_dest_path}"

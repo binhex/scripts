70dde8cff1b69f2c351ba553e53ed9172334a485
binhex <megalith01@gmail.com>
2025-07-18 14:57:17 +0100

    put in process names for all apps

--- scripts/docker/healthcheck.sh
+++ scripts/docker/healthcheck.sh
@@ -2,7 +2,7 @@
 
 # Script to check DNS resolution, HTTPS connectivity and processes, with optional custom
 # command and custom action defined via environment variables HEALTHCHECK_COMMAND and
-# HEALTHCHECK_ACTION.
+# HEALTHCHECK_ACTION and exit script with appropriate exit code.
 #
 # This script is called via the Dockerfile HEALTHCHECK instruction.
 
@@ -54,18 +54,107 @@ function check_process() {
 	fi
 
 	# convert app name into process name(s) to monitor
-	#
-	# note exceptions for monitoring are:
-	# get-iplayer - Process does not run all the time, thus unable to monitor
 	local process_names=()
-	if [[ "${APPNAME}" == 'deluge' ]]; then
+	if [[ "${APPNAME}" == 'bitmagnet' ]]; then
+		process_names=('bitmagnet')
+	elif [[ "${APPNAME}" == 'code-server' ]]; then
+		process_names=('code-server')
+	elif [[ "${APPNAME}" == 'crafty-4' ]]; then
+		process_names=('python.*crafty.*')
+	elif [[ "${APPNAME}" == 'deluge' ]]; then
 		process_names=('deluged' 'deluge-web')
 	elif [[ "${APPNAME}" == 'delugevpn' ]]; then
 		process_names=('deluged' 'deluge-web' 'openvpn|wg')
+	elif [[ "${APPNAME}" == 'emby' ]]; then
+		process_names=('EmbyServer')
+	elif [[ "${APPNAME}" == 'flaresolverr' ]]; then
+		process_names=('flaresolverr')
+	elif [[ "${APPNAME}" == 'fleet' ]]; then
+		process_names=('jetbrains-fleet')
+	elif [[ "${APPNAME}" == 'goland' ]]; then
+		process_names=('^/usr/sbin/goland')
+	elif [[ "${APPNAME}" == 'gonic' ]]; then
+		process_names=('gonic')
+	elif [[ "${APPNAME}" == 'hexchat' ]]; then
+		process_names=('^hexchat')
+	elif [[ "${APPNAME}" == 'intellij' ]]; then
+		process_names=('idea')
+	elif [[ "${APPNAME}" == 'jackett' ]]; then
+		process_names=('jackett')
+	elif [[ "${APPNAME}" == 'jellyfin' ]]; then
+		process_names=('jellyfin')
+	elif [[ "${APPNAME}" == 'jenkins' ]]; then
+		process_names=('jenkins')
+	elif [[ "${APPNAME}" == 'krusader' ]]; then
+		process_names=('^krusader')
+	elif [[ "${APPNAME}" == 'libreoffice' ]]; then
+		process_names=('^/usr/lib/libreoffice')
+	elif [[ "${APPNAME}" == 'lidarr' ]]; then
+		process_names=('Lidarr')
+	elif [[ "${APPNAME}" == 'makemkv' ]]; then
+		process_names=('^makemkv')
+	elif [[ "${APPNAME}" == 'medusa' ]]; then
+		process_names=('medusa')
+	elif [[ "${APPNAME}" == 'minecraftbedrockserver' ]]; then
+		process_names=('bedrock_server')
+	elif [[ "${APPNAME}" == 'minecraftserver' ]]; then
+		process_names=('^java.*minecraft.*')
+	elif [[ "${APPNAME}" == 'minidlna' ]]; then
+		process_names=('minidlnad')
+	elif [[ "${APPNAME}" == 'nginx' ]]; then
+		process_names=('nginx')
+	elif [[ "${APPNAME}" == 'nicotineplus' ]]; then
+		process_names=('^/usr/bin/python /usr/bin/nicotine')
+	elif [[ "${APPNAME}" == 'nzbget' ]]; then
+		process_names=('nzbget')
+	elif [[ "${APPNAME}" == 'nzbhydra2' ]]; then
+		process_names=('^java.*nzbhydra2.*')
+	elif [[ "${APPNAME}" == 'overseerr' ]]; then
+		process_names=('/usr/bin/node')
+	elif [[ "${APPNAME}" == 'plex' ]]; then
+		process_names=('Plex Media Server')
+	elif [[ "${APPNAME}" == 'plexpass' ]]; then
+		process_names=('Plex Media Server')
+	elif [[ "${APPNAME}" == 'privoxyvpn' ]]; then
+		process_names=('privoxy|microsocks' 'openvpn|wg')
+	elif [[ "${APPNAME}" == 'prowlarr' ]]; then
+		process_names=('Prowlarr')
+	elif [[ "${APPNAME}" == 'pycharm' ]]; then
+		process_names=('^/usr/share/pycharm')
 	elif [[ "${APPNAME}" == 'qbittorrent' ]]; then
 		process_names=('qbittorrent-nox')
 	elif [[ "${APPNAME}" == 'qbittorrentvpn' ]]; then
 		process_names=('qbittorrent-nox' 'openvpn|wg')
+	elif [[ "${APPNAME}" == 'radarr' ]]; then
+		process_names=('Radarr')
+	elif [[ "${APPNAME}" == 'rclone' ]]; then
+		process_names=('rclone')
+	elif [[ "${APPNAME}" == 'readarr' ]]; then
+		process_names=('Readarr')
+	elif [[ "${APPNAME}" == 'resilio-sync' ]]; then
+		process_names=('rslsync')
+	elif [[ "${APPNAME}" == 'rider' ]]; then
+		process_names=('^/usr/share/rider')
+	elif [[ "${APPNAME}" == 'rustrover' ]]; then
+		process_names=('^/opt/rustrover')
+	elif [[ "${APPNAME}" == 'sabnzbd' ]]; then
+		process_names=('python.*SABnzbd.*')
+	elif [[ "${APPNAME}" == 'sabnzbdvpn' ]]; then
+		process_names=('python.*SABnzbd.*' 'openvpn|wg')
+	elif [[ "${APPNAME}" == 'siphonator' ]]; then
+		process_names=('siphonator')
+	elif [[ "${APPNAME}" == 'sonarr' ]]; then
+		process_names=('Sonarr')
+	elif [[ "${APPNAME}" == 'syncthing' ]]; then
+		process_names=('syncthing')
+	elif [[ "${APPNAME}" == 'teamspeak' ]]; then
+		process_names=('ts3server')
+	elif [[ "${APPNAME}" == 'tvheadend' ]]; then
+		process_names=('tvheadend')
+	elif [[ "${APPNAME}" == 'urbackup' ]]; then
+		process_names=('urbackupsrv')
+	elif [[ "${APPNAME}" == 'webstorm' ]]; then
+		process_names=('^/opt/webstorm')
 	else
 		echo "[info] Application name '${APPNAME}' not in the known list for process monitoring."
 		return 0
@@ -74,7 +163,8 @@ function check_process() {
 	# loop over each process name in the array
 	for process_name in "${process_names[@]}"; do
 
-		if pgrep -x "${process_name}" > /dev/null; then
+		# -f flag matches $process_name against the full command line (partial match)
+		if pgrep -f "${process_name}" > /dev/null; then
 			echo "[info] Process '${process_name}' is running."
 		else
 			echo "[error] Process '${process_name}' is not running."

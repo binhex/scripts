9b83568ae78125d9c64ea58a2613dc5ef7b3e62e
binhex <megalith01@gmail.com>
2025-10-28 09:49:03 +0000

    update gh.sh

--- scripts/docker/gh.sh
+++ scripts/docker/gh.sh
@@ -13,15 +13,36 @@ function prereq() {
 		echo "[info] GitHub CLI 'gh' not found, installing..."
 		pacman -Sy --noconfirm
 		pacman -S github-cli --noconfirm
-	else
-		echo "[info] GitHub CLI 'gh' already installed"
 	fi
 
-	if [[ -z "${RELEASE_TYPE}" ]]; then
+	# check if git cli tool exists
+	if ! command -v git &> /dev/null; then
+		echo "[info] Git CLI 'git' not found, installing..."
+		pacman -Sy --noconfirm
+		pacman -S git --noconfirm
+	fi
+
+	if [[ -z "${DOWNLOAD_TYPE}" ]]; then
+		echo "[warn] GitHub download type not defined via parameter -dt or --download-type, displaying help..."
+		echo ""
+		show_help
+		exit 1
+	fi
+
+	if [[ "${DOWNLOAD_TYPE}" == 'release' && -z "${RELEASE_TYPE}" ]]; then
 		echo "[warn] GitHub release type not defined via parameter -rt or --release-type, displaying help..."
 		echo ""
 		show_help
 		exit 1
+	fi
+
+	if [[ "${DOWNLOAD_TYPE}" == 'release' && "${RELEASE_TYPE}" == 'binary' ]]; then
+		if [[ -z "${ASSET_GLOB}" ]]; then
+			echo "[warn] Asset glob not defined via parameter -ag or --asset-glob, displaying help..."
+			echo ""
+			show_help
+			exit 1
+		fi
 	fi
 
 	if [[ -z "${GITHUB_OWNER}" ]]; then
@@ -38,19 +59,11 @@ function prereq() {
 		exit 1
 	fi
 
-	if [[ "${RELEASE_TYPE}" == 'asset' ]]; then
-		if [[ -z "${ASSET_GLOB}" ]]; then
-			echo "[warn] Asset glob not defined via parameter -ag or --asset-glob, displaying help..."
-			echo ""
-			show_help
-			exit 1
-		fi
-	fi
 }
 
-function release_download_asset() {
+function release_download() {
 
-	if [[ "${RELEASE_TYPE}" == 'asset' ]]; then
+	if [[ "${RELEASE_TYPE}" == 'binary' ]]; then
 		type_flag="--pattern ${ASSET_GLOB}"
 	elif [[ "${RELEASE_TYPE}" == 'source' ]]; then
 		type_flag="--archive tar.gz"
@@ -61,11 +74,14 @@ function release_download_asset() {
 	# hoeever there is a hack/workaround for this that this script makes use of.
 	# detailed hack here: https://github.com/cli/cli/issues/2680#issuecomment-1345491083
 	GH_HOST=foobar gh release download -R "github.com/${GITHUB_OWNER}/${GITHUB_REPO}" --dir "${DOWNLOAD_PATH}" ${type_flag}
+
 }
 
 function repo_clone() {
+
 	mkdir -p "${DOWNLOAD_PATH}"
-	GH_HOST=foobar gh repo clone "${GITHUB_OWNER}/${GITHUB_REPO}" "${DOWNLOAD_PATH}" -- --depth=1
+	git clone "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}.git" "${DOWNLOAD_PATH}" --depth=1
+
 }
 
 function show_help() {
@@ -78,13 +94,21 @@ Where:
 	-h or --help
 		Displays this text.
 
-	-ag or --asset-glob <glob pattern>
-		Define the asset glob pattern to download (only applies when --release-type is 'asset')
-		No default.
-
 	-dp or --download-path <path>
 		Define path to download assets to.
 		Default is '${defaultDownloadPath}'.
+
+	-dt or --download-type <release|clone>
+		Define the type of download to perform.
+		No default.
+
+	-rt or --release-type <binary|source>
+		Define whether to download the release binary or release source.
+		No default.
+
+	-ag or --asset-glob <glob pattern>
+		Define the asset glob pattern to download (only applies when --release-type is 'binary')
+		No default.
 
 	-go or --github-owner <owner>
 		Define GitHub owners name.
@@ -94,19 +118,16 @@ Where:
 		Define GitHub repository name.
 		No default.
 
-	-rt or --release-type <asset|source>
-		Define whether to download the release asset or release source.
-		No default.
-
 Examples:
-	GitHub shallow clone of source code:
-	./${ourScriptName} --github-owner bitmagnet-io --github-repo bitmagnet --release-type source --download-path /home/nobody
+	GitHub release binary asset download:
+	./${ourScriptName} --github-owner zakkarry --github-repo deluge-ltconfig --download-type release --release-type binary --download-path /home/nobody --asset-glob '*.egg'
 
 	GitHub release source code asset download:
-	./${ourScriptName} --github-owner bitmagnet-io --github-repo bitmagnet --release-type asset --download-path /home/nobody --asset-glob '*.tar.gz'
+	./${ourScriptName} --github-owner bitmagnet-io --github-repo bitmagnet --download-type release --release-type source --download-path /home/nobody --asset-glob '*.tar.gz'
 
-	GitHub release binary asset download:
-	./${ourScriptName} --github-owner zakkarry --github-repo deluge-ltconfig --release-type asset --download-path /home/nobody --asset-glob '*.egg'
+		GitHub shallow clone of source code:
+	./${ourScriptName} --github-owner bitmagnet-io --github-repo bitmagnet --download-type clone --download-path /home/nobody
+
 ENDHELP
 }
 
@@ -114,12 +135,20 @@ while [ "$#" != "0" ]
 do
 	case "$1"
 	in
-		-ag|--asset-glob)
-			ASSET_GLOB=$2
-			shift
-			;;
 		-dp| --download-path)
 			DOWNLOAD_PATH=$2
+			shift
+			;;
+		-dt| --download-type)
+			DOWNLOAD_TYPE=$2
+			shift
+			;;
+		-rt|--release-type)
+			RELEASE_TYPE=$2
+			shift
+			;;
+		-ag|--asset-glob)
+			ASSET_GLOB=$2
 			shift
 			;;
 		-go|--github-owner)
@@ -128,10 +157,6 @@ do
 			;;
 		-gr|--github-repo)
 			GITHUB_REPO=$2
-			shift
-			;;
-		-rt|--release-type)
-			RELEASE_TYPE=$2
 			shift
 			;;
 		-h|--help)
@@ -149,11 +174,13 @@ done
 
 function main() {
 	prereq
-	if [[ "${RELEASE_TYPE}" == 'asset' ]]; then
-		release_download_asset
-	elif [[ "${RELEASE_TYPE}" == 'source' ]]; then
-		repo_clone
+
+	if [[ "${DOWNLOAD_TYPE}" == 'release' ]]; then
+			release_download
+	elif [[ "${DOWNLOAD_TYPE}" == 'clone' ]]; then
+			repo_clone
 	fi
+
 }
 
 main

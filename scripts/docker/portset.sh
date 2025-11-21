84abff72b533087c6f704b215fb737813c26e524
binhex <megalith01@gmail.com>
2025-11-21 13:47:29 +0000

    handle old and new api change for port forward

--- scripts/docker/portset.sh
+++ scripts/docker/portset.sh
@@ -235,7 +235,11 @@ function get_incoming_port() {
   # Get port forward information from gluetun Control Server
   portforward_response=$(curl_with_retry "${control_server_url}/portforward" 10 1 -s)
 
-  if [[ -z "${portforward_response}" || "${portforward_response}" == "Unauthorized" ]]; then
+  if [[ "${portforward_response}" == "Unauthorized" ]]; then
+    portforward_response=$(curl_with_retry "${control_server_url}/openvpn/portforwarded" 10 1 -s)
+  fi
+
+  if [[ -z "${portforward_response}" ]]; then
     echo "[WARN] Unable to retrieve port forwarded information from gluetun Control Server"
     INCOMING_PORT=""
   else

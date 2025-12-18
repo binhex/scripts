8cbf487ebb3db4ebe22e04cde6d0fe02594dd724
binhex <megalith01@gmail.com>
2025-12-18 09:21:52 +0000

    update user agent

--- scripts/docker/rcurl.sh
+++ scripts/docker/rcurl.sh
@@ -13,7 +13,7 @@ retry="${defaultRetry}"
 retry_delay="${defaultRetryDelay}"
 max_time="${defaultMaxTime}"
 
-header="user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
 
 function build_curl_command() {
 
